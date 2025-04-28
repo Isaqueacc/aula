946eef0501f0e2dcb533fd73c269e80eb81aa0de
Isaque Gonzaga <gonzagaisaque4@gmail.com>
2025-04-28 20:50:25 -0300

    Commit 05 Requisição de deletar usuario concluida.
    Task 1 : Ajustar o método de atualizar usuário
    Task 2 : Fazer o package de exceptions
    Task 3 : deixar o retorno ao excluir usuário, usuário já cadastrado e usuário não encontrado mais legivel
    Task 4 : ocultar a senha

--- testing/testing/src/main/java/com/aula/testing/controller/UserController.java
+++ testing/testing/src/main/java/com/aula/testing/controller/UserController.java
@@ -2,6 +2,8 @@ package com.aula.testing.controller;
 
 import com.aula.testing.models.UserEntity;
 import com.aula.testing.repository.UserRepo;
+import com.aula.testing.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,26 +11,41 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.management.relation.RelationNotFoundException;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/login")
 public class UserController {
     @Autowired
-    private UserRepo userRepositoy;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    public List<UserEntity> ListarTodos(){
-        return userRepositoy.findAll();
-
+    public  List<UserEntity> ListarTodos(){
+        return userService.ListAll();
     }
+
     @PostMapping
-    public ResponseEntity<UserEntity> save(@RequestBody UserEntity user) {
-    userRepositoy.save(user);
-    return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        public ResponseEntity<Map<String, Object>> save (@RequestBody UserEntity user){
+        userService.Save(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Map.of("message", "Usuário cadastrado com sucesso"));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String,Object>> atualizar(@Valid @RequestBody UserEntity userEntity){
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Message", "Usuario Atualizado com sucesso"));
     }
 
     @DeleteMapping("/{id}")
-    public void DeletarCliente(@PathVariable Long id){
-        userRepositoy.deleteById(id);
+    public ResponseEntity<Map<String, Object>> excluir(@PathVariable Long id){
+        userService.excluir(id);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("Menssagem", "Usuário excluido com sucesso"));
+
     }
+
 }
--- testing/testing/src/main/java/com/aula/testing/models/UserEntity.java
+++ testing/testing/src/main/java/com/aula/testing/models/UserEntity.java
@@ -1,6 +1,9 @@
 package com.aula.testing.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -14,9 +17,49 @@ public class UserEntity {
     @Id// Sinalizar que é um Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // gerar numero Id
     private Long id;
+    @NotBlank(message = "Nome é obrigatório")
     @Column(nullable = false) // não pode ser nulo
     private String nome;
+
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "E-mail deve ser válido")
     @Column(nullable = false)
     private String email;
-    //ORM
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 7, message = "A senha deve ter no minimo 7 caracteres")
+    @Column(nullable = false)
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
